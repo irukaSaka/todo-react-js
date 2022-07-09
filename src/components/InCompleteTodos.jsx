244c078c6b49d9417d38914dec1ff7c9aa06177b
Tanuki <80181268+irukaSaka@users.noreply.github.com>
2022-07-09 12:04:43 +0900

    style

--- src/components/InCompleteTodos.jsx
+++ src/components/InCompleteTodos.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-
+const style = {
+  backgroundColor: "#f5f0bb",
+  width: "400px",
+  minHeight: "200px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px"
+};
 export const InCompleteTodos = (props) => {
   const { todos, onClickComplete, onClickDelete } = props;
   return (
-    <div className="incomplete-area">
+    <div className="incomplete-area" style={style}>
       <p className="title">未完了のTODO</p>
       <ul>
         {todos.map((todo, index) => {
